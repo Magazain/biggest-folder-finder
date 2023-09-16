c9d0d8b062e2bc4e42025aa3eb0ce61ff163c1a0
Kirill Magazinov <lilstar.km@yandex.ru>
2023-09-16 22:18:32 +0300

    methods: getHumanReadableSize && getSizeFromHumanReadable

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,16 +12,41 @@ public class Main {
         ForkJoinPool pool = new ForkJoinPool();
         long size = pool.invoke(calculator);
         System.out.println(size);
+        System.out.println(getHumanReadableSize(size));
+        System.out.println(getSizeFromHumanReadable(getHumanReadableSize(size)));
     }
 
-    public static long getFolderSize(File folder){
-        if(folder.isFile()){
-            return folder.length();
+    public static String getHumanReadableSize(long size) {
+        int i = 4;
+        long newSize = Math.round(size / Math.pow(1024, i));
+
+        while (newSize < 1 && i >= 0) {
+            --i;
+            newSize = Math.round(size / Math.pow(1024, i));
         }
 
-        AtomicLong sum = new AtomicLong(0L);
-        List<File> files = List.of(folder.listFiles());
-        files.forEach(file -> sum.addAndGet((getFolderSize(file))));
-        return sum.get();
+        return newSize + switch (i) {
+            case 4 -> "Tb";
+            case 3 -> "Gb";
+            case 2 -> "Mb";
+            case 1 -> "Kb";
+            default -> "B";
+        };
+    }
+
+    public static long getSizeFromHumanReadable(String size) {
+        String regexSizeName = "[^A-Z]";
+        String regexNumber = "[^0-9]";
+
+        String sizeName = size.replaceAll(regexSizeName, "");
+        long numb = Long.parseLong(size.replaceAll(regexNumber, ""));
+
+        return switch (sizeName) {
+            case "T" -> (long) (numb * Math.pow(1024, 4));
+            case "G" -> (long) (numb * Math.pow(1024, 3));
+            case "M" -> (long) (numb * Math.pow(1024, 2));
+            case "K" -> (long) (numb * Math.pow(1024, 1));
+            default -> (long) (numb * Math.pow(1024, 0));
+        };
     }
 }
